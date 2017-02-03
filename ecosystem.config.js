f37d9efd4f029bdb6aaf630a5b61104465d0d804
Ola Adolfsson <Ola.Adolfsson@ist.com>
2017-02-03 19:44:13 +0100

    Added ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,56 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps: [
+
+    // First application
+    {
+      name: "aoo",
+      script: "server.js",
+      env: {
+        COMMON_VARIABLE: "true"
+      },
+      env_production: {
+        NODE_ENV: "production"
+      }
+    }
+
+    // // Second application
+    // {
+    //   name      : "WEB",
+    //   script    : "web.js"
+    // }
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy: {
+    production: {
+      user: "node",
+      host: "212.83.163.1",
+      ref: "origin/master",
+      repo: "git@github.com:repo.git",
+      path: "/var/www/production",
+      "post-deploy": "npm install && pm2 startOrRestart ecosystem.json --env production"
+    },
+    dev: {
+      user: "node",
+      host: [{
+        host: "192.168.99.100",
+        port: "22022"
+      }],
+      ref: "origin/master",
+      repo: "https://github.com/tibysko/herms-frontend.git",
+      path: "/home/node/development",
+      "post-deploy": "npm install && pm2 startOrRestart ecosystem.config.js --env dev",
+      env: {
+        NODE_ENV: "dev",
+        HTTP_PORT: "15000"
+      }
+    }
+  }
+}
