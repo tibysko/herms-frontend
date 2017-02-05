1722f4bcf5537f2974c98df16e8121df08aae0c3
Ola Adolfsson <Ola.Adolfsson@ist.com>
2017-02-05 20:55:52 +0100

    Updated websocket and api port

--- src/app/core/app-settings.ts
+++ src/app/core/app-settings.ts
@@ -1,4 +1,4 @@
 export class AppSettings {
-   public static API_ENDPOINT: string = 'http://' + window.location.hostname + ':8080/api';
-   public static WEBSOCKET_ENDPOINT: string = window.location.hostname + ':8081';
+   public static API_ENDPOINT: string = 'http://' + window.location.hostname + ':8081/api';
+   public static WEBSOCKET_ENDPOINT: string = window.location.hostname + ':8082';
 }
