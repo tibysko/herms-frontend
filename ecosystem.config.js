1955013ac2756f99dae7beca98300e6f5fbe2837
Ola Adolfsson <Ola.Adolfsson@ist.com>
2017-02-24 20:18:51 +0100

    Update with aws

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,20 +3,20 @@ module.exports = {
    * Application configuration section
    * http://pm2.keymetrics.io/docs/usage/application-declaration/
    */
-  apps: [
+  "apps": [
 
     // frontend application
     {
-      name: "frontend",
-      script: "server/server.js",
-      exec_mode: 'fork',
-      cwd: "/",
-      env: {
-        FRONTEND_PORT: "8080",
-        FRONTEND_PATH: "../dist"
+      "name": "frontend",
+      "script": "server/server.js",
+      "exec_mode": 'fork',
+      "cwd": "/",
+      "env": {
+        "FRONTEND_PORT": "8080",
+        "FRONTEND_PATH": "../dist"
       },
-      env_production: {
-        NODE_ENV: "production"
+      "env_production": {
+        "NODE_ENV": "production"
       }
     }
   ],
@@ -25,22 +25,26 @@ module.exports = {
    * Deployment section
    * http://pm2.keymetrics.io/docs/usage/deployment/
    */
-  deploy: {
-    production: {
-      user: "pi", 
-      host: [{
-        host: "192.168.1.146", // raspberry
-        port: "22" 
+  "deploy": {
+    "production": {
+      "user": "pi",
+      "host": [{
+        "host": "192.168.1.146", // raspberry
+        "port": "22"
       }],
-      ref: "origin/master",
-      repo: "https://github.com/tibysko/herms-frontend.git",
-      path: "/home/pi/herms/frontend",
+      "ref": "origin/master",
+      "repo": "https://github.com/tibysko/herms-frontend.git",
+      "path": "/home/pi/herms/frontend",
       "post-deploy": "npm install && npm run build-prod && pm2 startOrRestart ecosystem.config.js --env production",
-      env: {
-        NODE_ENV: "production",
-        FRONTEND_PORT: "8080",
-        FRONTEND_PATH: "dist"
-      }
+    },
+    "aws": {
+      "user": "ubuntu",
+      "host": "52.11.194.229",
+      "port": "22",
+      "ref": "origin/master",
+      "repo": "https://github.com/tibysko/herms-frontend.git",
+      "path": "/home/ubuntu/herms/frontend",
+      "post-deploy": "npm install && npm run build-prod && pm2 startOrRestart ecosystem.config.js --env production",
     }
   }
 }
